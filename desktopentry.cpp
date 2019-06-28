5da10127b51882b70b9baeba318971a535e45a24
chenghaopeng <i@chper.cn>
2019-06-28 17:47:25 +0800

    🍔🍔 maybe completed DesktopEntryIO.

--- desktopentry.cpp
+++ desktopentry.cpp
@@ -2,6 +2,13 @@
 
 namespace DesktopEntryItem {
 
+    int getTypeByKey(QString key) {
+        if (key == KEY_NAME || key == KEY_EXEC || key == KEY_TYPE || key == KEY_ICON || key == KEY_GENERICNAME || key == KEY_COMMENT || key == KEY_VERSION) return 1;
+        if (key == KEY_TERMINAL) return 2;
+        if (key == KEY_CATEGORIES) return 3;
+        return 0;
+    }
+
     //Item
     bool Item::itemKeyIs(QString key) {
         return this->key == key;
@@ -50,8 +57,8 @@ namespace DesktopEntryItem {
         this->list.append(value);
         this->value = list.join(";");
     }
-    QList<QString> ListItem::getListValue() {
-        return this->list;
+    QList<QString>* ListItem::getListValue() {
+        return new QList<QString>(this->list);
     }
 
 }
@@ -75,7 +82,59 @@ bool DesktopEntry::getBooleanValue(QString key) {
     DesktopEntryItem::BooleanItem* item = static_cast<DesktopEntryItem::BooleanItem*>(DesktopEntry::getItemByKey(key));
     return item->getBooleanValue();
 }
-QList<QString> DesktopEntry::getListValue(QString key) {
+QList<QString>* DesktopEntry::getListValue(QString key) {
     DesktopEntryItem::ListItem* item = static_cast<DesktopEntryItem::ListItem*>(DesktopEntry::getItemByKey(key));
     return item->getListValue();
+}
+void DesktopEntry::append(DesktopEntryItem::Item *item) {
+    this->items.append(item);
+}
+
+namespace DesktopEntryIO {
+    bool saveToFile(DesktopEntry* desktopEntry, QString fileName) {
+        QString str = DESKTOP_ENTRY;
+        foreach(DesktopEntryItem::Item* item, desktopEntry->items) {
+            str += "\n" + item->toString();
+        }
+        QByteArray byteArray = str.toUtf8();
+        QFile file(fileName);
+        if (!file.open(QIODevice::WriteOnly | QIODevice::Text))
+            return false;
+        qint64 res = file.write(byteArray, byteArray.length());
+        file.close();
+        return res != -1;
+    }
+    bool readFromFile(DesktopEntry* desktopEntry, QString fileName) {
+        QFile file(fileName);
+        try {
+            if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+                throw;
+            QTextStream in(&file);
+            in.setAutoDetectUnicode(true);
+            QString str = in.readLine();
+            if (str.simplified().toLower() != DESKTOP_ENTRY) {
+                throw;
+            }
+            while (!in.atEnd()) {
+                str = in.readLine().trimmed();
+                int pos = str.indexOf("=");
+                if (pos < 0) throw;
+                QString key = str.left(pos + 1), value = str.right(str.size() - pos - 1);
+                if (key.contains("[")) throw;
+                int typeOfKey = DesktopEntryItem::getTypeByKey(key);
+                if (typeOfKey == 0) throw;
+                DesktopEntryItem::Item* item;
+                if (typeOfKey == 1) item = new DesktopEntryItem::TextItem(key, value);
+                if (typeOfKey == 2) item = new DesktopEntryItem::BooleanItem(key, value);
+                if (typeOfKey == 3) item = new DesktopEntryItem::ListItem(key, value);
+                desktopEntry->items.append(item);
+            }
+            file.close();
+            return true;
+        }
+        catch(...) {
+            file.close();
+            return false;
+        }
+    }
 }
--- desktopentry.h
+++ desktopentry.h
@@ -3,11 +3,14 @@
 
 #include <QString>
 #include <QList>
+#include <QTextStream>
 
 namespace DesktopEntryItem {
     static const QString KEY_NAME = "Name", KEY_EXEC = "Exec", KEY_TYPE = "Type";
     static const QString KEY_TERMINAL = "Terminal", KEY_ICON = "Icon", KEY_GENERICNAME = "GenericName";
     static const QString KEY_COMMENT = "Comment", KEY_VERSION = "Version", KEY_CATEGORIES = "Categories";
+
+    int getTypeByKey(QString key);
 
     class Item {
     protected:
@@ -42,19 +45,26 @@ namespace DesktopEntryItem {
     public:
         ListItem(QString key, QString value);
         ListItem(QString key, QList<QString> value);
-        QList<QString> getListValue();
+        QList<QString>* getListValue();
     };
 }
 
 class DesktopEntry {
 private:
-    QList<DesktopEntryItem::Item*> items;
     DesktopEntryItem::Item* getItemByKey(QString key);
 public:
+    QList<DesktopEntryItem::Item*> items;
     DesktopEntry();
     QString getValue(QString key);
     bool getBooleanValue(QString key);
-    QList<QString> getListValue(QString key);
+    QList<QString>* getListValue(QString key);
+    void append(DesktopEntryItem::Item* item);
+};
+
+namespace DesktopEntryIO {
+    const static QString DESKTOP_ENTRY = "[Desktop Entry]";
+    bool saveToFile(DesktopEntry* desktopEntry, QString fileName);
+    bool readFromFile(DesktopEntry* desktopEntry, QString fileName);
 };
 
 #endif // DESKTOPENTRY_H
